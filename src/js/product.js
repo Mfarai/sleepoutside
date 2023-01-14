42d00ee89238191b5d93d9019da552a392727354
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-01-13 20:54:49 -0700

    Creating ProductDetails and updating product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-
+import ProductDetails from "./ProductDetails.mjs";
 const dataSource = new ProductData("tents");
 let cartItems = [];
 function addProductToCart(product) {
