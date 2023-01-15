57be4e17c053bcb4a9b2d818b3fb49c8a328e408
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-01-14 20:25:03 -0700

    Working on Individual Dynamic Product List

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
 import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductListing.mjs";
 // Gets all products information with URL
 const dataSource = new ProductData("tents");
+const listing = new ProductListing("category", dataSource, "<P>")
