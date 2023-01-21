9286b67895d5a3d64ad65d4b2f2cecc3a23727ad
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-01-21 10:06:16 -0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,16 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-
+// Gets all products information with URL
 const dataSource = new ProductData("tents");
+// Finds the element location for the product cards
 const element = document.querySelector(".product-list");
+// Creates the listing
 const listing = new ProductList("Tents", dataSource, element);
 
 listing.init();
 
-// Gets all products information with URL
 
-// Finds the element location for the product cards
 
-// Creates the listing
+
+
+
