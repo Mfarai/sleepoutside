2bbfd9214575b49b83af441f8eff0922173a6ae3
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-01-24 20:40:05 -0700

    Creating Checkout form

--- src/js/cart.js
+++ src/js/cart.js
@@ -17,8 +17,51 @@ function renderCartContents() {
     for (let i = 0; i < cartItems.length; i++) {
       totalPrice += cartItems[i].FinalPrice;
     }
+    var shippingEst = 2.5;
+    var taxPercent = .06;
+    var taxAmount = totalPrice * taxPercent;
+    var orderTotal = shippingEst + taxAmount + totalPrice;
 
-    document.querySelector(".cart-total").innerHTML = `Total: ` + totalPrice;
+    document.querySelector(".cart-total").innerHTML = `
+    <section class="checkoutForm">
+    <form>
+    <h3> Customer Name </h3>
+    <div>
+      <label for="fname">First name:</label><br>
+      <input type="text" id="fname" name="fname" required><br>
+      <label for="lname">Last name:</label><br>
+      <input type="text" id="lname" name="lname" required>
+    </div>
+    <h3> Address </h3>
+    <div>
+      <label for="sAddress">Street Address:</label><br>
+      <input type="text" id="sAddress" name="sAddress" required><br>
+      <label for="city">City:</label><br>
+      <input type="text" id="city" name="city" required><br>
+      <label for="state">State:</label><br>
+      <input type="text" id="state" name="state" required><br>
+      <label for="zipCode">Zip Code</label><br>
+      <input type="text" id="zipCode" name="zipCode" required>
+    </div>
+    <h3> Payment </h3>
+    <div>
+      <label for="ccNum">Credit Card Number:</label><br>
+      <input type="text" id="ccNum" name="ccNum" required><br>
+      <label for="exDate">Expiration Date:</label><br>
+      <input type="text" id="exDate" name="exDate" required><br>
+      <label for="secCode">Security Code:</label><br>
+      <input type="text" id="secCode" name="secCode"><br>
+    </div>
+    </form>
+  </section> 
+  <section class="orderSummary">
+  <p>Subtotal: `+ Math.round((totalPrice + Number.EPSILON) * 100) / 100 + `</p>
+  <p>Shipping Estimate: `+  Math.round((shippingEst + Number.EPSILON) * 100) / 100 + `</p>
+  <p>Tax: `+ Math.round((taxAmount + Number.EPSILON) * 100) / 100 + `</p>
+  <p>Order Total: `+ Math.round((orderTotal + Number.EPSILON) * 100) / 100  + `</p>
+  <button class="checkOutButton" type="button">Check Out</button>
+  </section>`;
+ 
   }
 }
 
