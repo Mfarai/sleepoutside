c64fdf0cdbdd92b2f8aeddc8563c5ac40455ac2d
Katie Blunck <97479562+katelyndb@users.noreply.github.com>
2023-01-30 19:09:38 -0700

    Worked on it all day. Tried

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,12 @@ import { loadHeaderFooter } from "./utils.mjs";
 
 // Creates the header and footer
 loadHeaderFooter();
+
+// Katie on Site open Message
+
+if (localStorage.getItem("newVisitor") == null) {
+    alert("AGH");
+  document.querySelector(".welcome-message").classList.remove("hide");
+  localStorage.setItem("newVisitor", "done");
+  localStorage.clear();
+}
